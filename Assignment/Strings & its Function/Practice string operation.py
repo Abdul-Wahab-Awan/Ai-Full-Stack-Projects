b72659519e338aa871cd343da4e7261dee39f12b
Abdul Wahab Awan <167404782+Abdul-Wahab-Awan@users.noreply.github.com>
2026-09-10 03:03:54 +0500

    Add files via upload

---
+++ Assignment/Strings & its Function/Practice string operation.py
@@ -0,0 +1,9 @@
+LahoreInfo="LAhore is the capital of Lahore and Islamabad is the Capital of Pakistan";
+print(LahoreInfo)
+print(type(LahoreInfo))
+print(LahoreInfo.capitalize())
+print(LahoreInfo.upper())
+print(LahoreInfo.lower())
+print(LahoreInfo.swapcase())
+print(LahoreInfo.title())
+print(LahoreInfo.count())
---
+++ Assignment/Strings & its Function/String practice.py
@@ -0,0 +1,8 @@
+a="Ismail Sahab"
+# slicing string
+# What are indexing
+#[I][S][M] ->Indexing
+# 0 1 2 
+print(a[:6])
+print(a[2:6])
+print(a[::])
